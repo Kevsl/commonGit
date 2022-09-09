66454239bec5002a3578a60b1278b54583063c38
Rabieh Sassi <sassi_r@etna-alternance.net>
2022-09-09 14:01:59 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Route to="/">Home</Route>
-          </li>
-          <li>
-            <Route to="/about">About</Route>
-          </li>
-          <li>
-            <Route to="/contact">Contact</Route>
-          </li>
-        </ul>
-      </div>
-    </Router>
+    <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+    </div>
   );
 }
 
