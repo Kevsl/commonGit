0ec752e2a3f8bfb1e4eac90bfe7b7a4d92354bc8
Kevin Terrier <kevin.terrier@le1817.com>
2022-09-09 15:03:20 +0200

    added menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import {Routes, Route} from "react-router-dom";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
+import { Routes, Route, Link } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import './App.css'
 
 function App() {
-
   return (
     <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <ul className="flex mx-auto items-center w-1-2 bg-dark-blue justify-around text-white rounded-md">
+        <Link to="">
+          <li className="text-white">Users</li>
+        </Link>
+        <Link to="">
+          <li className="text-white">Flags</li>
+        </Link>
+        <Link to="">
+          <li className="text-white">Movies</li>
+        </Link>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
